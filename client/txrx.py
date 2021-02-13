6eac2c54a7c58ce32fff1db0df862a9ad7b858a2
Joe Hitchen <hitchenjoe+git@gmail.com>
2021-02-13 23:13:00 +0000

    Moved image generation to second-layer asynchronous task

--- client/txrx.py
+++ client/txrx.py
@@ -1,6 +1,6 @@
 import os
 
-from celery import Celery
+from celery import Celery, result
 
 import image_utils as utils
 
@@ -16,19 +16,35 @@ txrx = Celery(
 @txrx.task(name = 'txrx.docker_pull')
 def request_image_transfer(image_name):
     
-    image_short = image_name.split('/')[1]
-    image_filename = image_short.replace(':', '.') + '.docker'
-     
+    image_name_repoless = '/'.join(image_name.split('/')[1:])
+    
+    with result.allow_join_result():
+        image_meta = transfer_docker_image(image_name_repoless).wait()
+    assert image_meta['name'] == image_name_repoless
+    
+    utils.load_and_push(
+        image_name,
+        os.path.join(RX_DATA, image_meta['file']),
+        image_meta['hash'],
+    )
+
+
+def transfer_docker_image(image_name):
+    return _transfer_docker_image.apply_async((image_name,), countdown = 15)
+
+
+@txrx.task(name = 'txrx.transfer_docker_image')
+def _transfer_docker_image(image_name):
+    
+    image_filename = image_name.replace(':', '-') + '.docker'
+    
     image_meta = utils.build_and_save(
-        image_short,
+        image_name,
         os.path.join(RX_DATA, image_filename),
         'mock',
     )
     
-    utils.load_and_push(
-        image_name,
-        os.path.join(RX_DATA, image_filename),
-        image_meta['hash'],
-    )
+    image_meta['file'] = image_filename
+    return image_meta
 
 
