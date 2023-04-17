138e3134a3ea8d6165f56d6ddec9005881a2a942
Joe Hitchen <hitchenjoe+git@gmail.com>
2023-04-17 23:13:13 +0100

    Improved image and container display boxes

--- registry-ui/server.py
+++ registry-ui/server.py
@@ -8,18 +8,29 @@ serverPort = 8080
 
 
 def create_image_box(image: utils.Image) -> str:
-    """Helper function for creating the HTML to display individual images."""
+    """Helper function for displaying a note if there are no images."""
     
     box_styling = 'd-flex justify-content-between align-items-center'
     return '''
-          <div class="list-group-item list-group-item-action {box_styling}">
-            <div class="fs-3 mb-2">{image.name}:{image.tag}</div>
+          <div class="list-group-item list-group-item-action {box_styling} p-2">
+            <div class="fs-4 font-monospace ms-2">{image.name}:{image.tag}</div>
             <div>
-              <div>{image.digest:.20s}</div>
+              <div class="lh-1">{image.digest:.20s}</div>
               <div class="small text-muted text-end">{image.created}</div>
             </div>
           </div>
     '''.format(image = image, box_styling = box_styling)
+
+
+def no_images_box() -> str:
+    """Helper function for creating the HTML to display individual images."""
+    
+    box_styling = 'd-flex justify-content-between align-items-center'
+    return '''
+          <div class="list-group-item list-group-item-action {box_styling} p-2">
+            <div class="fs-5 ms-2">{message}</div>
+          </div>
+    '''.format(message = 'No images to show', box_styling = box_styling)
 
 
 def create_container_box(container: utils.Container) -> str:
@@ -27,20 +38,38 @@ def create_container_box(container: utils.Container) -> str:
     
     status_colour = 'success' if container.status == 'running' else 'danger'
     return '''
-          <div class="list-group-item list-group-item-action {box_styling}">
-            <div class="mb-2">
+          <div class="list-group-item list-group-item-action {box_styling} p-2">
+            <div>
               <span style="width: 1em; height: 1em" class="{status_styling}"></span>
-              <span class="fs-3">{container.name}</span>
+              <span class="fs-4 font-monospace">{container.name}</span>
             </div>
             <div>
-              <div>{container.image}</div>
+              <div class="lh-1">{container.image}</div>
               <div class="small text-muted text-end">{container.created}</div>
             </div>
           </div>
     '''.format(
         container = container,
         box_styling = 'd-flex justify-content-between align-items-center',
-        status_styling = f'bg-{status_colour} d-inline-block rounded-circle',
+        status_styling = f'bg-{status_colour} d-inline-block rounded-circle mx-2',
+    )
+
+
+def no_containers_box() -> str:
+    """Helper function for displaying a note if there are no containers."""
+    
+    status_colour = 'danger'
+    return '''
+          <div class="list-group-item list-group-item-action {box_styling} p-2">
+            <div>
+              <span style="width: 1em; height: 1em" class="{status_styling}"></span>
+              <span class="fs-5">{message}</span>
+            </div>
+          </div>
+    '''.format(
+        message = 'No containers to show',
+        box_styling = 'd-flex justify-content-between align-items-center',
+        status_styling = f'bg-{status_colour} d-inline-block rounded-circle mx-2',
     )
 
 
@@ -56,9 +85,13 @@ class RegistryUiServer(BaseHTTPRequestHandler):
             verify_ssl = os.environ.get('REGISTRY_VERIFY_SSL', '').lower() != 'false',
         )
         image_boxes = [create_image_box(image) for image in images]
+        if not image_boxes:
+            image_boxes = [no_images_box()]
         
         containers = utils.get_containers(os.environ.get('DOCKER_HOST', ''))
         container_boxes = [create_container_box(container) for container in containers]
+        if not container_boxes:
+            container_boxes = [no_containers_box()]
         
         # Generate full page
         with open('registry_page.html') as file:
