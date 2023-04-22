d6b2eaa0f43f21ebfdb2d68dc995f6560625e068
Joe Hitchen <hitchenjoe+git@gmail.com>
2023-04-22 22:17:45 +0100

    Refactored container and image display to separate functions

--- dashboard/server.py
+++ dashboard/server.py
@@ -22,15 +22,24 @@ def create_image_box(image: utils.Image) -> str:
     '''.format(image = image, box_styling = box_styling)
 
 
-def no_images_box() -> str:
-    """Helper function for creating the HTML to display individual images."""
+def create_image_boxes() -> list[str]:
+    """Loads and generates the HTML for the registry images."""
     
-    box_styling = 'd-flex justify-content-between align-items-center'
-    return '''
+    images = utils.get_registry_images(
+        'https://' + os.environ.get('REGISTRY_HOST', ''),
+        verify_ssl = os.environ.get('REGISTRY_VERIFY_SSL', '').lower() != 'false',
+    )
+    if images:
+        return [create_image_box(image) for image in images]
+    
+    return ['''
           <div class="list-group-item list-group-item-action {box_styling} p-2">
             <div class="fs-5 ms-2">{message}</div>
           </div>
-    '''.format(message = 'No images to show', box_styling = box_styling)
+    '''.format(
+        message = 'No images to show',
+        box_styling = 'd-flex justify-content-between align-items-center',
+    )]
 
 
 def create_container_box(container: utils.Container) -> str:
@@ -55,22 +64,26 @@ def create_container_box(container: utils.Container) -> str:
     )
 
 
-def no_containers_box() -> str:
-    """Helper function for displaying a note if there are no containers."""
+def create_container_boxes() -> list[str]:
+    """Loads and generates the HTML for the server containers."""
     
-    status_colour = 'danger'
-    return '''
-          <div class="list-group-item list-group-item-action {box_styling} p-2">
-            <div>
-              <span style="width: 1em; height: 1em" class="{status_styling}"></span>
-              <span class="fs-5">{message}</span>
-            </div>
-          </div>
+    containers = utils.get_containers(os.environ.get('DOCKER_HOST', ''))
+    
+    if containers:
+        return [create_container_box(container) for container in containers]
+    
+    return ['''
+      <div class="list-group-item list-group-item-action {box_styling} p-2">
+        <div>
+          <span style="width: 1em; height: 1em" class="{status_styling}"></span>
+          <span class="fs-5">{message}</span>
+        </div>
+      </div>
     '''.format(
         message = 'No containers to show',
         box_styling = 'd-flex justify-content-between align-items-center',
-        status_styling = f'bg-{status_colour} d-inline-block rounded-circle mx-2',
-    )
+        status_styling = 'bg-danger d-inline-block rounded-circle mx-2',
+    )]
 
 
 class RegistryUiServer(BaseHTTPRequestHandler):
@@ -79,25 +92,16 @@ class RegistryUiServer(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
         """Handle all GET requests."""
         
-        # Generate image boxes
-        images = utils.get_registry_images(
-            'https://' + os.environ.get('REGISTRY_HOST', ''),
-            verify_ssl = os.environ.get('REGISTRY_VERIFY_SSL', '').lower() != 'false',
-        )
-        image_boxes = [create_image_box(image) for image in images]
-        if not image_boxes:
-            image_boxes = [no_images_box()]
-        
-        containers = utils.get_containers(os.environ.get('DOCKER_HOST', ''))
-        container_boxes = [create_container_box(container) for container in containers]
-        if not container_boxes:
-            container_boxes = [no_containers_box()]
+        self.index()
+    
+    
+    def index(self) -> None:
         
         # Generate full page
         with open('registry_page.html') as file:
             page = file.read().format(
-                registry_contents = ''.join(image_boxes),
-                containers = ''.join(container_boxes),
+                registry_contents = ''.join(create_image_boxes()),
+                containers = ''.join(create_container_boxes()),
             )
         
         # Send reponse
