15afd277f386db68bfa54e61fb083d1fc2328b3d
Joe Hitchen <hitchenjoe+git@gmail.com>
2023-04-30 12:54:17 +0100

    Refactored HTML display to use Jinja templates

--- dashboard/server.py
+++ dashboard/server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 import utils
 
@@ -12,26 +12,13 @@ server = Flask(__name__)
 def index() -> str:
     """Creates the main dashboard page."""
     
-    with open('registry_page.html') as file:
-        return file.read().format(
-            registry_contents = create_image_boxes(),
-            containers = create_container_boxes(),
-        )
-
-
-def create_image_box(image: utils.Image) -> str:
-    """Helper function for displaying a note if there are no images."""
+    images = utils.get_registry_images(
+        'https://' + os.environ.get('REGISTRY_HOST', ''),
+        verify_ssl = os.environ.get('REGISTRY_VERIFY_SSL', '').lower() != 'false',
+    )
+    containers = utils.get_containers(os.environ.get('DOCKER_HOST', ''))
     
-    box_styling = 'd-flex justify-content-between align-items-center'
-    return '''
-          <div class="list-group-item list-group-item-action {box_styling} p-2">
-            <div class="fs-4 font-monospace ms-2">{image.name}:{image.tag}</div>
-            <div>
-              <div class="lh-1">{image.digest:.20s}</div>
-              <div class="small text-muted text-end">{image.created}</div>
-            </div>
-          </div>
-    '''.format(image = image, box_styling = box_styling)
+    return render_template('index_page.html', images = images, containers = containers)
 
 
 @server.route('/images/')
@@ -42,39 +29,7 @@ def create_image_boxes() -> str:
         'https://' + os.environ.get('REGISTRY_HOST', ''),
         verify_ssl = os.environ.get('REGISTRY_VERIFY_SSL', '').lower() != 'false',
     )
-    if images:
-        return ''.join(create_image_box(image) for image in images)
-    
-    return '''
-          <div class="list-group-item list-group-item-action {box_styling} p-2">
-            <div class="fs-5 ms-2">{message}</div>
-          </div>
-    '''.format(
-        message = 'No images to show',
-        box_styling = 'd-flex justify-content-between align-items-center',
-    )
-
-
-def create_container_box(container: utils.Container) -> str:
-    """Helper function for creating the HTML to display individual containers."""
-    
-    status_colour = 'success' if container.status == 'running' else 'danger'
-    return '''
-          <div class="list-group-item list-group-item-action {box_styling} p-2">
-            <div>
-              <span style="width: 1em; height: 1em" class="{status_styling}"></span>
-              <span class="fs-4 font-monospace">{container.name}</span>
-            </div>
-            <div>
-              <div class="lh-1">{container.image}</div>
-              <div class="small text-muted text-end">{container.created}</div>
-            </div>
-          </div>
-    '''.format(
-        container = container,
-        box_styling = 'd-flex justify-content-between align-items-center',
-        status_styling = f'bg-{status_colour} d-inline-block rounded-circle mx-2',
-    )
+    return render_template('images.html', images = images)
 
 
 @server.route('/containers/')
@@ -82,20 +37,5 @@ def create_container_boxes() -> str:
     """Loads and generates the HTML for the server containers."""
     
     containers = utils.get_containers(os.environ.get('DOCKER_HOST', ''))
-    
-    if containers:
-        return ''.join(create_container_box(container) for container in containers)
-    
-    return '''
-      <div class="list-group-item list-group-item-action {box_styling} p-2">
-        <div>
-          <span style="width: 1em; height: 1em" class="{status_styling}"></span>
-          <span class="fs-5">{message}</span>
-        </div>
-      </div>
-    '''.format(
-        message = 'No containers to show',
-        box_styling = 'd-flex justify-content-between align-items-center',
-        status_styling = 'bg-danger d-inline-block rounded-circle mx-2',
-    )
+    return render_template('containers.html', containers = containers)
 
